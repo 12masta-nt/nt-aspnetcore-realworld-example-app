48a442263a16b56a9f129f0a956992b129a75929
marcinstanek <stanek.marcinp@gmail.com>
2020-01-29 21:51:56 +0100

    init

---
+++ src/Conduit/Features/Users/UsersController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conduit.Features.Users
+{
+    [Route("users")]
+    public class UsersController
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<UserEnvelope> Create([FromBody] Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete]
+        public async Task<UserEnvelope> Delete([FromBody] Delete.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPost("login")]
+        public async Task<UserEnvelope> Login([FromBody] Login.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
